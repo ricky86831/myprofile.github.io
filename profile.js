a2ab48176f4ab7ecfd45cd8bc65c2a0c2f43b1e8
ricky86831 <ricky86831@gmail.com>
2017-10-30 19:10:35 +0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,26 +1,27 @@
- ("section").hover(function () {
-     (this).addClass("hover");
-     (this).siblings().addClass("unhover");
-}, function () {
-     (this).removeClass("hover");
-     (this).siblings().removeClass("unhover");
+  ("section").hover(function () {
+      (this).addClass("hover");
+      (this).siblings().addClass("unhover");
+},
+     function () {
+      (this).removeClass("hover");
+      (this).siblings().removeClass("unhover");
 });
 
- ("section").click(function () {
-     (this).addClass("click");
-     (this).children('div').delay(300).fadeIn(150);
-     (this).siblings().addClass("unclick");
-     (this).siblings().children("p").fadeOut(50);
+  ("section").click(function () {
+      (this).addClass("click");
+      (this).children('div').delay(300).fadeIn(150);
+      (this).siblings().addClass("unclick");
+      (this).siblings().children("p").fadeOut(50);
 });
 
- (".x").click(function (event) {
+  (".x").click(function (event) {
     event.stopPropagation();
-     ("section").removeClass("click unclick");
-     ("section").siblings().children('div').fadeOut(150);
-     ("section").siblings().children('p').fadeIn(50);
+      ("section").removeClass("click unclick");
+      ("section").siblings().children('div').fadeOut(150);
+      ("section").siblings().children('p').fadeIn(50);
 });
 
- (document).keydown(function (e) {
+  (document).keydown(function (e) {
     switch (e.which) {
         case 27:
             reset();
@@ -44,7 +45,7 @@
 });
 
 function open(nr) {
-    var t =  ("section:nth-child(" + nr + ")");
+    var t =   ("section:nth-child(" + nr + ")");
     reset();
     t.toggleClass("click");
     t.children('div').delay(300).fadeToggle(300);
@@ -53,7 +54,7 @@ function open(nr) {
 }
 
 function reset() {
-     ("section").removeClass("click unclick");
-     ("section").siblings().children('div').fadeOut(150);
-     ("section").siblings().children('p').fadeIn(50);
+      ("section").removeClass("click unclick");
+      ("section").siblings().children('div').fadeOut(150);
+      ("section").siblings().children('p').fadeIn(50);
 }
